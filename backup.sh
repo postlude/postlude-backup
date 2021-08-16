faaa62d3435d9f88f0b7d327f3d7b44ef45ba0ab
“postlude <useseungdeokhan@naver.com>
2021-08-16 23:58:16 +0900

    modify shell

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@ CUR_DATE=`date "+%Y%m%d%H%M%S"`
 SQL_FILE="backup_$CUR_DATE.sql"
 TAR_FILE="backup_$CUR_DATE.tar.gz"
 
-mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_USER_PASSWORD postlude > /$SQL_FILE
+mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_USER_PASSWORD --add-drop-database --no-tablespaces postlude > /$SQL_FILE
 
 tar cvf $MOUNT_PATH/$TAR_FILE /$SQL_FILE
