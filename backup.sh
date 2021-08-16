0d0e628f6bd0acf431f5d4591246294faa646e8c
“postlude <useseungdeokhan@naver.com>
2021-08-16 22:04:57 +0900

    modify backup.sh

--- backup.sh
+++ backup.sh
@@ -6,4 +6,4 @@ TAR_FILE="backup_$CUR_DATE.tar.gz"
 
 mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p $MYSQL_USER_PASSWORD postlude > /$SQL_FILE
 
-tar cvf $MOUNT_PATH/$TAR_FILE $SQL_FILE
+tar cvf $MOUNT_PATH/$TAR_FILE /$SQL_FILE
